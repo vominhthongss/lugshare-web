fc749085d0a867013e2bebf87346b4a34e734553
vominhthongss <vominhthongss@gmail.com>
2023-12-02 13:50:34 +0900

    update footer.js and not-found.js

--- src/pages/not-found/not-found.js
+++ src/pages/not-found/not-found.js
@@ -1,5 +1,19 @@
+import { Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 function NotFound() {
-  return <>404</>;
+  return (
+    <div className="w-full flex justify-center h-[70vh] items-center">
+      <div className="flex flex-col space-y-4 items-center ">
+        <span className="text-7xl">404</span>
+        <span>Ooops!!</span>
+        <span>THAT PAGE DOESN'T EXIST OR IS UNAVAILABLE.</span>
+        <NavLink to={"/home"}>
+          <Button variant="contained">Back To Home</Button>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
