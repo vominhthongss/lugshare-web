42d0e9a3156e6433b7216856862fdf7469ff650e
vominhthongss <vominhthongss@gmail.com>
2023-12-02 13:38:15 +0900

    update footer.js and navigation.js

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,12 +1,18 @@
 import { Button } from "@mui/material";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
 function Navigation() {
   return (
     <div className="bg-main flex flex-row justify-between p-2 shadow-md">
-      <Link to={"/home"}>
+      <NavLink to={"/home"}>
         <span className="text-3xl font-bold">Lug Share</span>
-      </Link>
+      </NavLink>
 
       <div>
         <Button variant="outlined">Log in</Button>
