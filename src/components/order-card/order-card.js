701d52a0e484fb6955de3257cd7e5985e5ccf2ad
vominhthongss <vominhthongss@gmail.com>
2024-01-10 20:27:16 +0900

    update order-card.js

--- src/components/order-card/order-card.js
+++ src/components/order-card/order-card.js
@@ -1,21 +1,58 @@
-import Skeleton from '@mui/material/Skeleton';
+import Skeleton from "@mui/material/Skeleton";
 function OrderCard({ order, index }) {
   return (
     <div className="bg-white p-4 rounded shadow-md hover:shadow-lg border cursor-pointer">
-      <h2 className="text-xl font-bold mb-2">Order {index + 1}</h2>
+      <h2 className="text-xl font-bold mb-2">
+        {order.title ?? (
+          <Skeleton variant="text" sx={{ fontSize: "1rem", width: "100%" }} />
+        )}
+      </h2>
       <div className="mb-2 flex space-x-2">
         <p>Recipient:</p>{" "}
         <p className="hover:underline cursor-pointer hover:text-blue-500">
-          {order.owner_first_name && order.owner_last_name ? `${order.owner_first_name} ${order.owner_last_name}` : <Skeleton variant="text" sx={{ fontSize: '1rem', width: '100%' }} />}
+          {order.owner_first_name && order.owner_last_name ? (
+            `${order.owner_first_name} ${order.owner_last_name}`
+          ) : (
+            <Skeleton variant="text" sx={{ fontSize: "1rem", width: "100%" }} />
+          )}
         </p>
       </div>
-      <p className="mb-2 flex">✈️ {order.departure ?? <Skeleton variant="text" sx={{ fontSize: '1rem', width: '100%' }} />}</p>
+      <p className="mb-2 flex">
+        ✈️{" "}
+        {order.departure_location ?? (
+          <Skeleton variant="text" sx={{ fontSize: "1rem", width: "100%" }} />
+        )}
+      </p>
       <p>↓</p>
-      <p className="mb-2 flex">⛳️ {order.destination ?? <Skeleton variant="text" sx={{ fontSize: '1rem', width: '100%' }} />}</p>
-      <p className="mb-2 flex">⏱️ {order.posted_time ?? <Skeleton variant="text" sx={{ fontSize: '1rem', width: '100%' }} />}</p>
+      <p className="mb-2 flex">
+        ⛳️{" "}
+        {order.arrival_location ?? (
+          <Skeleton variant="text" sx={{ fontSize: "1rem", width: "100%" }} />
+        )}
+      </p>
+      <p className="mb-2 flex">
+        ⏱️{" "}
+        {order.posted_time ?? (
+          <Skeleton variant="text" sx={{ fontSize: "1rem", width: "100%" }} />
+        )}
+      </p>
       <div className="flex flex-row space-x-2">
         <p>🚚 Remain Weight:</p>{" "}
-        <p className="text-green-700 font-bold">{order.delivery_fee ?? <Skeleton variant="text" sx={{ fontSize: '1rem', width: '100%' }} />} kg</p>
+        <p className="text-green-700 font-bold">
+          {order.remaining_weight ?? (
+            <Skeleton variant="text" sx={{ fontSize: "1rem", width: "100%" }} />
+          )}{" "}
+          kg
+        </p>
+      </div>
+      <div className="flex flex-row space-x-2">
+        <p>💵 Delivery fee:</p>{" "}
+        <p className="text-green-700 font-bold">
+          {order.delivery_fee ?? (
+            <Skeleton variant="text" sx={{ fontSize: "1rem", width: "100%" }} />
+          )}{" "}
+          $
+        </p>
       </div>
     </div>
   );
